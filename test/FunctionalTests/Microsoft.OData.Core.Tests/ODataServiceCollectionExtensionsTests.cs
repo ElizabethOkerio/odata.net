2fe3bacb8739ef64d4e3853ae75257d153abb60d
Robert McLaws <1657085+robertmclaws@users.noreply.github.com>
2024-03-18 23:04:27 +0000

    - Refactor AddDefaultODataServices to leverage optional constructor parameters and allow for configuring default OData-related settings without injecting new settings instances into the container.
    - Deleted ContainerBuilderExtensionsTests because they don't actually test anything related to OData.
    
    - Created new unit tests that specifically test the various permitations of AddDefaultODataServices to make sure they function correctly without abdicating that responsibility to end-to-end tests.

---
+++ test/FunctionalTests/Microsoft.OData.Core.Tests/ODataServiceCollectionExtensionsTests.cs
@@ -0,0 +1,67 @@
+﻿//---------------------------------------------------------------------
+// <copyright file="BufferUtilsTests.cs" company="Microsoft">
+//      Copyright (C) Microsoft Corporation. All rights reserved. See License.txt in the project root for license information.
+// </copyright>
+//---------------------------------------------------------------------
+
+using System;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OData.Edm;
+using Microsoft.OData.Json;
+using Microsoft.OData.UriParser;
+using Xunit;
+
+namespace Microsoft.OData.Tests
+{
+
+    /// <summary>
+    /// Tests methods related to registering OData services in Dependency Injection containers.
+    /// </summary>
+    public class ODataServiceCollectionExtensionsTests
+    {
+
+        [Fact]
+        public void AddDefaultODataServices_RegistersServicesCorrectly()
+        {
+            var services = new ServiceCollection();
+            Assert.Empty(services);
+
+            services.AddDefaultODataServices();
+            Assert.True(services.Count == 11);
+
+            var provider = services.BuildServiceProvider();
+            Assert.NotNull(provider);
+
+            // @robertmclaws: Test for registered Singletons.
+            Assert.NotNull(provider.GetService<IJsonReaderFactory>());
+            Assert.NotNull(provider.GetService<IJsonWriterFactory>());
+            Assert.NotNull(provider.GetService<ODataMediaTypeResolver>());
+            Assert.NotNull(provider.GetService<ODataPayloadValueConverter>());
+            Assert.NotNull(provider.GetService<IEdmModel>());
+            Assert.NotNull(provider.GetService<ODataUriResolver>());
+
+            // @robertmclaws: Test for request-scoped services.
+            var scope = provider.CreateScope();
+            Assert.NotNull(scope);
+            Assert.NotNull(scope.ServiceProvider);
+
+            Assert.NotNull(scope.ServiceProvider.GetService<ODataMessageInfo>());
+            Assert.NotNull(scope.ServiceProvider.GetService<UriPathParser>());
+            Assert.NotNull(scope.ServiceProvider.GetService<ODataMessageReaderSettings>());
+            Assert.NotNull(scope.ServiceProvider.GetService<ODataMessageWriterSettings>());
+            Assert.NotNull(scope.ServiceProvider.GetService<ODataUriParserSettings>());
+        }
+
+        // [Fact]
+        // public void RentFromBufferShouldThrowsIfNullBufferReturns()
+        // {
+        //     // Arrange
+        //     Action test = () => BufferUtils.RentFromBuffer(new BadCharArrayPool(), 1024);
+
+        //     // Act & Assert
+        //     var exception = Assert.Throws<ODataException>(test);
+        //     Assert.Equal(Strings.BufferUtils_InvalidBufferOrSize(1024), exception.Message);
+        // }
+
+    }
+}
