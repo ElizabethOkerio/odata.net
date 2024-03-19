976dc684e8fab19d80cfebc15fcb034820108c16
Elizabeth Okerio <elokerio@microsoft.com>
2024-03-19 09:34:17 +0300

    update changes

--- src/Microsoft.OData.Core/ODataServiceCollectionExtensions.cs
+++ src/Microsoft.OData.Core/ODataServiceCollectionExtensions.cs
@@ -1,5 +1,5 @@
 ﻿//---------------------------------------------------------------------
-// <copyright file="ODataServiceColectionExtensions.cs" company="Microsoft">
+// <copyright file="ODataServiceCollectionExtensions.cs" company="Microsoft">
 // Copyright (C) Microsoft Corporation. All rights reserved. See License.txt in the project root for license information.
 // </copyright>
 //---------------------------------------------------------------------
